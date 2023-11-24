dbf29a3103666c4bd2d6315af54d12280a0fd859
Lahiru Bandara <lahirub@ceyentra.com>
2023-11-24 22:13:32 +0530

    Initial Commit

---
+++ Test/HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() 
+{
+    std::cout << "Hello World!\n"; 
+    std::cin.get(); // Waits for a keypress
+    
+    return 0;
+}
+
+// How to compile: g++ HelloWorld.cpp -o HelloWorld.exe
+// How to run: ./HelloWorld.exe
